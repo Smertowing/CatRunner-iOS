7796c369a0081001c23ce65de9c9c3b6ad5e264b
Kiryl <Kiryl.co@gmail.com>
2018-10-28 23:28:07 +0300

    Files

---
+++ CatRunner/Model/History.swift
@@ -0,0 +1,9 @@
+//
+//  History.swift
+//  CatRunner
+//
+//  Created by Kiryl Holubeu on 10/28/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import Foundation
---
+++ CatRunner/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  CatRunner
+//
+//  Created by Kiryl Holubeu on 10/28/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
