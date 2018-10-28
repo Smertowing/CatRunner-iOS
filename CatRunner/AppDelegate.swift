4ab590508387021a233f7a37b07c6eafe932a4fc
Kiryl <Kiryl.co@gmail.com>
2018-10-28 23:09:09 +0300

    Main navigation

--- CatRunner/AppDelegate.swift
+++ CatRunner/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyCzIa1a92gfYro6hzrvGRJe5y72zhDruPc")
+        GMSPlacesClient.provideAPIKey("AIzaSyCzIa1a92gfYro6hzrvGRJe5y72zhDruPc")
         return true
     }
 
