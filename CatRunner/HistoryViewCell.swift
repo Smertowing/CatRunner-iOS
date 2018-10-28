c7e508a0e7b248b8f70c2c6757cc73739d11ea27
Kiryl <Kiryl.co@gmail.com>
2018-10-29 00:07:29 +0300

    Icons

---
+++ CatRunner/HistoryViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryViewCell.swift
+//  CatRunner
+//
+//  Created by Kiryl Holubeu on 10/28/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
