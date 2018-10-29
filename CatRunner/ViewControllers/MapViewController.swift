876c0337b3809adea7470b6e5327c577d97d3735
Kiryl <Kiryl.co@gmail.com>
2018-10-30 00:05:46 +0300

    Maps API test

---
+++ CatRunner/ViewControllers/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  CatRunner
+//
+//  Created by Kiryl Holubeu on 10/30/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+
+}
