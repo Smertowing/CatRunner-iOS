c7d2ccc60ba5ae426f91d49cc80dd6d3fa0303e5
Kiryl <Kiryl.co@gmail.com>
2018-11-01 16:09:38 +0300

    Map embedding

---
+++ CatRunner/View Controllers/MainTabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarViewController.swift
+//  CatRunner
+//
+//  Created by Kiryl Holubeu on 11/1/18.
+//  Copyright © 2018 Kiryl Holubeu. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+    }
+
+}
